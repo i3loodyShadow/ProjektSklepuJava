9864ca83a5d4a13ee7d38d912f6f7bde57e1672d
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2021-01-02 19:59:19 +0100

    Update WartoscParametrow.java

--- sklep_elektroniczny_ejb/ejbModule/com/sklep/entities/WartoscParametrow.java
+++ sklep_elektroniczny_ejb/ejbModule/com/sklep/entities/WartoscParametrow.java
@@ -22,12 +22,12 @@ public class WartoscParametrow implements Serializable {
 
 	//bi-directional many-to-one association to NazwaParametrow
 	@ManyToOne
-	@JoinColumn(name="id_nazwa_parametrow")
+	@JoinColumn(name="id_nazwa_parametrow", insertable = false, updatable = false)
 	private NazwaParametrow nazwaParametrow;
 
 	//bi-directional many-to-one association to Towar
 	@ManyToOne
-	@JoinColumn(name="id_towar")
+	@JoinColumn(name="id_towar", insertable = false, updatable = false)
 	private Towar towar;
 
 	public WartoscParametrow() {
