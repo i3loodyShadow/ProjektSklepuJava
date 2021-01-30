2e7a01454fd127da097bf135462f8d76a88b0f36
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2021-01-30 18:50:52 +0100

    Update SzczegTowaruBB.java

--- sklep_elektroniczny_web/src/com/sklep/towar/SzczegTowaruBB.java
+++ sklep_elektroniczny_web/src/com/sklep/towar/SzczegTowaruBB.java
@@ -1,31 +1,93 @@
 package com.sklep.towar;
 
-import javax.enterprise.context.RequestScoped;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.List;
+
+import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.faces.context.Flash;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.http.HttpSession;
 
-public class SzczegTowaruBB {
+import com.sklep.dao.TowarDAO;
+import com.sklep.entities.Towar;
+import com.sklep.entities.WartoscParametrow;
+
+@Named
+@ViewScoped
+public class SzczegTowaruBB implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private static final String PAGE_MAIN = "/public/towarList?faces-redirect=true";
+	private static final String PAGE_STAY_AT_THE_SAME = null;
+
+	private Towar towar = new Towar();
+	private Towar loaded = new Towar();
+	private String prod;
+	private String mod;
+	private List<WartoscParametrow> list;
+
+	@EJB
+	TowarDAO TowarDAO;
+
+	@Inject
+	FacesContext context;
+
+	@Inject
+	Flash flash;
+
+	public Towar getTowar() {
+		return towar;
+	}
 	
-	@Named
-	@RequestScoped
-	public class MessagesView {
-	     
-	    public void info() {
-	        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "PrimeFaces Rocks."));
-	    }
-	     
-	    public void warn() {
-	        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Warning!", "Watch out for PrimeFaces."));
-	    }
-	     
-	    public void error() {
-	        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "Contact admin."));
-	    }
-	     
-	    public void fatal() {
-	        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Fatal!", "System Error"));
-	    }
+	public String getProd() {
+		return prod;
+	}
+
+	public void setProd(String prod) {
+		this.prod = prod;
+	}
+
+	public String getMod() {
+		return mod;
+	}
+
+	public void setMod(String mod) {
+		this.mod = mod;
+	}
+
+	public List<WartoscParametrow> getList() {
+		return list;
+	}
+
+	public void setList(List<WartoscParametrow> list) {
+		this.list = list;
+	}
+
+	public void onLoad() throws IOException {
+
+		loaded = (Towar) flash.get("towar");
+
+		if (loaded != null) {
+			towar = loaded;
+
+			int idTowaru = towar.getIdtowar();
+			
+			setProd(towar.getProducent());
+			
+			setMod(towar.getModel());
+			
+			list = TowarDAO.getTowarDetails(idTowaru).getWartoscParametrows();
+			
+			//System.out.println(list);
+
+		} else {
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "B³êdne u¿ycie systemu!", null));
+		}
 	}
 
 }
