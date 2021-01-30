2a4f033604b5319c14943650bc1933edce595b51
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2021-01-30 18:50:24 +0100

    Create KoszykBB.java

---
+++ sklep_elektroniczny_web/src/com/sklep/koszyk/KoszykBB.java
@@ -0,0 +1,16 @@
+package com.sklep.koszyk;
+
+import javax.faces.simplesecurity.RemoteClient;
+
+public class KoszykBB {
+	
+	public String dodajDoKoszyka() {
+		
+		
+		
+		String ddk = "cos";
+		
+		return ddk;
+	}
+
+}
