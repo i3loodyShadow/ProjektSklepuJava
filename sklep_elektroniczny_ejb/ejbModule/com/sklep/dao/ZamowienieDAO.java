e4e332595f7a607e1d90ed0c9b8d3cac268d8dc0
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2021-02-02 19:37:09 +0100

    Update ZamowienieDAO.java

--- sklep_elektroniczny_ejb/ejbModule/com/sklep/dao/ZamowienieDAO.java
+++ sklep_elektroniczny_ejb/ejbModule/com/sklep/dao/ZamowienieDAO.java
@@ -3,14 +3,17 @@ package com.sklep.dao;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
+import com.sklep.entities.Konto;
 import com.sklep.entities.Zamowienie;
 
 @Stateless
 public class ZamowienieDAO {
+	private final static String UNIT_NAME = "sklep-simplePU";
 	
-	@PersistenceContext
-	EntityManager em;
+	@PersistenceContext(unitName = UNIT_NAME)
+	protected EntityManager em;
 	
 	public void insert(Zamowienie zamowienie) {
 		em.persist(zamowienie);
@@ -26,5 +29,36 @@ public class ZamowienieDAO {
 
 	public Zamowienie get(Object id) {
 		return em.find(Zamowienie.class, id);
+	}
+	
+	public void createZamowienie(Integer cena, Konto idKonto) {
+		
+		Zamowienie z = new Zamowienie();
+		
+		z.setStatus(1);
+		z.setKoszt(cena.toString());
+		z.setKonto(idKonto);
+		
+		insert(z);
+	}
+	
+	public int getSpecificIdZamowienie(Konto konto) {
+		int idZamowienie;
+		
+		try {
+			
+			Query query = em.createQuery("from Zamowienie z where z.konto_idkonto=:idKonto");
+			
+			query.setParameter("idKonto", konto);
+			
+			idZamowienie = (int)query.getSingleResult();
+			
+			return idZamowienie;
+		} catch (javax.persistence.NoResultException e) {
+			idZamowienie = 0;
+			return idZamowienie;
+		}
+		
+		
 	}
 }
