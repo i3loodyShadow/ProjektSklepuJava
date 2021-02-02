6795af0199633d661b818103f67371b1804665e6
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2021-02-02 19:42:43 +0100

    Update KoszykBB.java

--- sklep_elektroniczny_web/src/com/sklep/koszyk/KoszykBB.java
+++ sklep_elektroniczny_web/src/com/sklep/koszyk/KoszykBB.java
@@ -1,12 +1,22 @@
 package com.sklep.koszyk;
 
-import javax.faces.simplesecurity.RemoteClient;
+import javax.faces.context.Flash;
+import javax.inject.Inject;
 
 public class KoszykBB {
 	
+	int idKonto;
+	
+	@Inject
+	Flash flash;
+	
 	public String dodajDoKoszyka() {
 		
+		idKonto = (int) flash.get("idKonto");
 		
+		if(idKonto != 0) {
+			
+		}
 		
 		String ddk = "cos";
 		
