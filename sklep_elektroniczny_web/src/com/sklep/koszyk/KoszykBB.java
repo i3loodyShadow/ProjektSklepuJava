655b3c7b985cc620563929d9321d5b32e77c75cb
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2021-02-10 19:27:58 +0100

    Update KoszykBB.java

--- sklep_elektroniczny_web/src/com/sklep/koszyk/KoszykBB.java
+++ sklep_elektroniczny_web/src/com/sklep/koszyk/KoszykBB.java
@@ -1,26 +1,74 @@
 package com.sklep.koszyk;
 
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.context.Flash;
 import javax.inject.Inject;
 
+import com.sklep.entities.Towar;
+import com.sklep.entities.Zamowienie;
+
 public class KoszykBB {
 	
-	int idKonto;
+	private Zamowienie zamowienie = new Zamowienie();
+	private Zamowienie loaded = new Zamowienie();
+	private String prod;
+	private String mod;
+	private int idKonto;
+	private List<Zamowienie> list;
 	
 	@Inject
 	Flash flash;
 	
-	public String dodajDoKoszyka() {
-		
-		idKonto = (int) flash.get("idKonto");
-		
-		if(idKonto != 0) {
-			
-		}
-		
-		String ddk = "cos";
-		
-		return ddk;
+	@Inject
+	FacesContext context;
+
+	public String getProd() {
+		return prod;
 	}
+
+	public void setProd(String prod) {
+		this.prod = prod;
+	}
+
+	public String getMod() {
+		return mod;
+	}
+
+	public void setMod(String mod) {
+		this.mod = mod;
+	}
+
+	public int getIdKonto() {
+		return idKonto;
+	}
+
+	public void setIdKonto(int idKonto) {
+		this.idKonto = idKonto;
+	}
+	public List<Zamowienie> getList() {
+		return list;
+	}
+
+	public void setList(List<Zamowienie> list) {
+		this.list = list;
+	}
+	
+	public String onLoad() {
+		String onLoad = "1";
+	
+/*		if (loaded != null) {
+			towar = loaded;
+			
+		} else {
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "B³êdne u¿ycie systemu!", null));
+		}
+*/		return onLoad;
+	}
+
+
+
 
 }
