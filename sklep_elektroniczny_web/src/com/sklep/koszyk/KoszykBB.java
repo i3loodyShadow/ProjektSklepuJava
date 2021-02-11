16fb95c5c20d6d7ace2bb26f2e16dc291fefde83
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2021-02-11 18:54:49 +0100

    Update KoszykBB.java

--- sklep_elektroniczny_web/src/com/sklep/koszyk/KoszykBB.java
+++ sklep_elektroniczny_web/src/com/sklep/koszyk/KoszykBB.java
@@ -1,29 +1,45 @@
 package com.sklep.koszyk;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.faces.application.FacesMessage;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.context.Flash;
 import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
-import com.sklep.entities.Towar;
+import com.sklep.dao.KontoDAO;
+import com.sklep.dao.ZamowienieDAO;
+import com.sklep.entities.TowarZamowienia;
 import com.sklep.entities.Zamowienie;
 
+@Named
+@RequestScoped
 public class KoszykBB {
+	private static final String PAGE_KOSZYK = "/public/koszyk?faces-redirect=true";
 	
-	private Zamowienie zamowienie = new Zamowienie();
-	private Zamowienie loaded = new Zamowienie();
 	private String prod;
 	private String mod;
 	private int idKonto;
 	private List<Zamowienie> list;
+	private List<Integer> listIdZ = new ArrayList<Integer>();
+	private List<TowarZamowienia> tz = new ArrayList<TowarZamowienia>();
 	
 	@Inject
 	Flash flash;
 	
 	@Inject
 	FacesContext context;
+	
+	@EJB
+	KontoDAO kontoDAO;
+	
+	@EJB
+	ZamowienieDAO zamowienieDAO;
 
 	public String getProd() {
 		return prod;
@@ -56,19 +72,58 @@ public class KoszykBB {
 		this.list = list;
 	}
 	
-	public String onLoad() {
-		String onLoad = "1";
-	
-/*		if (loaded != null) {
-			towar = loaded;
-			
-		} else {
-			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "B³êdne u¿ycie systemu!", null));
-		}
-*/		return onLoad;
+	public List<Integer> getListIdZ() {
+		return listIdZ;
 	}
 
+	public void setListIdZ(List<Integer> listIdZ) {
+		this.listIdZ = listIdZ;
+	}
+	
+	public List<TowarZamowienia> getTz() {
+		return tz;
+	}
 
+	public void setTz(List<TowarZamowienia> tz) {
+		this.tz = tz;
+	}
+	
+	public int getIdKontoFromSession() {
+		
+		FacesContext ctx = FacesContext.getCurrentInstance();
+		HttpServletRequest request = (HttpServletRequest) ctx.getExternalContext().getRequest();
+		HttpSession session = (HttpSession) request.getSession(true);
+		int idK = (int)session.getAttribute("idKonto");
+		
+		return idK;
+	}
+	
+	public void onLoad() {
+		
+		idKonto = getIdKontoFromSession();
+		
+		list = kontoDAO.getZamowienieDetails(idKonto).getZamowienies();
+		
+		for (int i = 0; i<list.size(); i++) {
+			
+			Zamowienie z = list.get(i);
+			
+			listIdZ.add(z.getIdzamowienie());
+				
+		}
+		
+		for (int i = 0; i<listIdZ.size(); i++){
+			
+			tz.addAll(zamowienieDAO.getTowarZamowieniaDetails(listIdZ.get(i)).getTowarZamowienias());
+			
+		}
 
+	}
+	
+	
+
+	public String doKoszyka() {
+		return PAGE_KOSZYK;
+	}
 
 }
