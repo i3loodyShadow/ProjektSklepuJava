317fd4838ab7f8776e7fe6568ebcfac39ef63016
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2021-02-23 19:08:19 +0100

    Update ZamowienieDAO.java

--- sklep_elektroniczny_ejb/ejbModule/com/sklep/dao/ZamowienieDAO.java
+++ sklep_elektroniczny_ejb/ejbModule/com/sklep/dao/ZamowienieDAO.java
@@ -3,16 +3,19 @@ package com.sklep.dao;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
+import com.sklep.entities.Konto;
 import com.sklep.entities.Zamowienie;
 
 @Stateless
 public class ZamowienieDAO {
+	private final static String UNIT_NAME = "sklep-simplePU";
 	
-	@PersistenceContext
+	@PersistenceContext(unitName = UNIT_NAME)
 	EntityManager em;
 	
-	public void insert(Zamowienie zamowienie) {
+	public void create(Zamowienie zamowienie) {
 		em.persist(zamowienie);
 	}
 
@@ -26,5 +29,36 @@ public class ZamowienieDAO {
 
 	public Zamowienie get(Object id) {
 		return em.find(Zamowienie.class, id);
+	}
+	
+	public Zamowienie createZamowienie(String cena, Konto konto) {
+		
+		Zamowienie z = new Zamowienie();
+		
+		z.setStatus(1);
+		z.setKoszt(cena);
+		z.setKonto(konto);
+		
+		create(z);
+		
+		return z;
+	}
+	
+	public Zamowienie getTowarZamowieniaDetails(int idZ){
+	
+		Zamowienie z = em.find(Zamowienie.class, idZ);	
+		z.getTowarZamowienias().size();
+		
+		return z;
+	}
+	
+	public void deleteZamowienieById(int id) {
+		
+		Query query = em.createQuery("delete from Zamowienie z where z.idzamowienie=:idZamowienie");
+		
+		query.setParameter("idZamowienie",id);
+		
+		query.executeUpdate();
+		
 	}
 }
