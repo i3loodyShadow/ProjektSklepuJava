bcc6aeed9b8ac12abbd9e7e9fce9bd596b04b604
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2021-02-23 19:08:09 +0100

    Update TowarZamowieniaDAO.java

--- sklep_elektroniczny_ejb/ejbModule/com/sklep/dao/TowarZamowieniaDAO.java
+++ sklep_elektroniczny_ejb/ejbModule/com/sklep/dao/TowarZamowieniaDAO.java
@@ -5,13 +5,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import com.sklep.entities.TowarZamowienia;
+import com.sklep.entities.Zamowienie;
 
 @Stateless
-
 public class TowarZamowieniaDAO {
+	private final static String UNIT_NAME = "sklep-simplePU";
 	
-	@PersistenceContext
-	EntityManager em;
+	@PersistenceContext(unitName = UNIT_NAME)
+	protected EntityManager em;
 	
 	public void insert(TowarZamowienia towarZamowienia) {
 		em.persist(towarZamowienia);
@@ -28,5 +29,19 @@ public class TowarZamowieniaDAO {
 	public TowarZamowienia get(Object id) {
 		return em.find(TowarZamowienia.class, id);
 	}
-
+	
+	public TowarZamowienia createKoszyk(String cena, String producent, String model, Zamowienie z) {
+		
+		TowarZamowienia t = new TowarZamowienia();
+		
+		t.setZamowienie(z);
+		t.setProducent(producent);
+		t.setModel(model);
+		t.setCena(cena);
+		
+		update(t);
+		
+		return t;
+	}
+	
 }
