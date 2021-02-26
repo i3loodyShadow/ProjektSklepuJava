ed0e68236df72b81530db30f24c8a5c932dd5666
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2021-02-26 21:49:38 +0100

    Update GrupyTowarowDAO.java

--- sklep_elektroniczny_ejb/ejbModule/com/sklep/dao/GrupyTowarowDAO.java
+++ sklep_elektroniczny_ejb/ejbModule/com/sklep/dao/GrupyTowarowDAO.java
@@ -1,10 +1,15 @@
 package com.sklep.dao;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import com.sklep.entities.GrupyTowarow;
+import com.sklep.entities.Konto;
+import com.sklep.entities.NazwaParametrow;
 
 @Stateless
 public class GrupyTowarowDAO {
@@ -27,4 +32,44 @@ public class GrupyTowarowDAO {
 	public GrupyTowarow get(Object id) {
 		return em.find(GrupyTowarow.class, id);
 	}
+	
+	public List<NazwaParametrow> getNPList(){
+		
+		List<NazwaParametrow> np;
+		
+		try {
+			
+			Query query = em.createQuery("select nazwaGrupy from GrupyTowarow g");
+			
+			np = query.getResultList();
+			return np ;
+
+		} catch (Exception e) {
+			np = null;
+			
+			return np;
+		}
+		
+	}
+	
+	public GrupyTowarow getGTByName(String name) {
+		
+		GrupyTowarow g = new GrupyTowarow();
+		
+		try {
+			
+			Query query = em.createQuery("from GrupyTowarow g where g.nazwaGrupy=:nazwaGrupy");
+			
+			query.setParameter("nazwaGrupy",name);
+			
+			g = (GrupyTowarow)query.getSingleResult();
+			return g ;
+
+		} catch (Exception e) {
+			g = null;
+			
+			return g;
+		}
+	}
+	
 }
