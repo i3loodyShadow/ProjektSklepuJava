b7a6a4ce7e789fb552d98bb85ae9763d36e2e805
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2021-02-28 20:37:54 +0100

    Update NazwaParametrowDAO.java

--- sklep_elektroniczny_ejb/ejbModule/com/sklep/dao/NazwaParametrowDAO.java
+++ sklep_elektroniczny_ejb/ejbModule/com/sklep/dao/NazwaParametrowDAO.java
@@ -3,13 +3,15 @@ package com.sklep.dao;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import com.sklep.entities.NazwaParametrow;
 
 @Stateless
 public class NazwaParametrowDAO {
+	private final static String UNIT_NAME = "sklep-simplePU";
 
-	@PersistenceContext
+	@PersistenceContext(unitName = UNIT_NAME)
 	EntityManager em;
 	
 	public void insert(NazwaParametrow nazwaParametrow) {
@@ -28,5 +30,19 @@ public class NazwaParametrowDAO {
 		return em.find(NazwaParametrow.class, id);
 	}
 	
-	
+	public NazwaParametrow getObjectByName(String name) {
+		try {
+			
+			Query query = em.createQuery("from NazwaParametrow n where n.nazwaParametru=:name");
+			
+			query.setParameter("name", name);
+			
+			NazwaParametrow n = (NazwaParametrow)query.getSingleResult();
+			
+			return n;
+		} catch (javax.persistence.NoResultException e) {
+			NazwaParametrow n = null;
+			return n;
+		}
+	}
 }
