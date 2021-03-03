460948da62421da641fa51c6863f4b4ea837374e
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2021-03-03 20:57:27 +0100

    Update NazwaParametrowDAO.java

--- sklep_elektroniczny_ejb/ejbModule/com/sklep/dao/NazwaParametrowDAO.java
+++ sklep_elektroniczny_ejb/ejbModule/com/sklep/dao/NazwaParametrowDAO.java
@@ -1,10 +1,13 @@
 package com.sklep.dao;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import com.sklep.entities.GrupyTowarow;
 import com.sklep.entities.NazwaParametrow;
 
 @Stateless
@@ -14,29 +17,27 @@ public class NazwaParametrowDAO {
 	@PersistenceContext(unitName = UNIT_NAME)
 	EntityManager em;
 	
-	public void insert(NazwaParametrow nazwaParametrow) {
+	public void create(NazwaParametrow nazwaParametrow) {
 		em.persist(nazwaParametrow);
 	}
 
-	public NazwaParametrow update(NazwaParametrow nazwaParametrow) {
+	public NazwaParametrow merge(NazwaParametrow nazwaParametrow) {
 		return em.merge(nazwaParametrow);
 	}
 
-	public void delete(NazwaParametrow nazwaParametrow) {
+	public void remove(NazwaParametrow nazwaParametrow) {
 		em.remove(em.merge(nazwaParametrow));
 	}
 
-	public NazwaParametrow get(Object id) {
+	public NazwaParametrow find(Object id) {
 		return em.find(NazwaParametrow.class, id);
 	}
 	
 	public NazwaParametrow getObjectByName(String name) {
 		try {
 			
-			Query query = em.createQuery("from NazwaParametrow n where n.nazwaParametru=:name");
-			
+			Query query = em.createQuery("from NazwaParametrow n where n.nazwaParametru=:name");	
 			query.setParameter("name", name);
-			
 			NazwaParametrow n = (NazwaParametrow)query.getSingleResult();
 			
 			return n;
@@ -44,5 +45,32 @@ public class NazwaParametrowDAO {
 			NazwaParametrow n = null;
 			return n;
 		}
+	}
+	
+	public List<String> getNPList(){
+		List<String> NPList = null;
+		
+		try {
+			Query query = em.createQuery("select nazwaParametru from NazwaParametrow n");
+			NPList = query.getResultList();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return NPList;
+	}
+	
+	public List<GrupyTowarow> pobierzGTDanejNP(String nazwaParametru){
+		List<GrupyTowarow> GTDanejNPList = null;
+		
+		try {
+			Query query = em.createQuery("select grupyTowarows from NazwaParametrow n where n.nazwaParametru=:NazwaParametru");
+			query.setParameter("NazwaParametru", nazwaParametru);
+			GTDanejNPList = query.getResultList();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return GTDanejNPList;
 	}
 }
